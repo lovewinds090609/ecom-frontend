7db1c4694906f749e2f7b1321630d0cdc2b65560
Maple <lovewinds090609@gmail.com>
2025-05-25 15:27:24 +0800

    重構 App 組件以引入 Products 組件，新增 Filter、Pagination、ProductCard、Products 和 ProductViewModel 組件，並添加產品資料顯示邏輯。

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { FaBeer } from 'react-icons/fa';
-function App() {
-  const [count, setCount] = useState(0)
+import Products from './components/Products';
 
+function App() {
   return (
-    <div className='flex items-center justify-center h-screen bg-gray-800 text-white text-2x1 font-bold'>
-      Welcome <FaBeer />?
+    <div>
+      <Products />
     </div>
   )
 }
